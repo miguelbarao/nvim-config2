928d9aec10e498cb0cf10b86748f2c479879fb28
Miguel Barão <mjsb@uevora.pt>
2026-09-09 13:15:27 +0100

    add markdown code fences

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,5 +4,16 @@ vim.b.minisurround_config = {
     l = {
       output = { left = '![](', right = ')' },
     },
+    -- markdown code fence (code is indented)
+    -- FIXME: always indents content (treesitter?)
+    c = {
+      output = { left = '```', right = '```' },
+    },
+    C = {
+      output = function()
+        local lang = MiniSurround.user_input('Language')
+        return { left = '```' .. lang, right = '```' }
+      end,
+    }
   },
 }
